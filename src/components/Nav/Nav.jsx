c0af55df2eefc492a135abd15d46e5f72d39074b
Sarah <sarahdavis398@gmail.com>
2022-12-01 19:24:18 +1000

    styling navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,9 +9,8 @@ function Nav() {
             <nav className="left-menu">
                 <Link className="button" to="/">Cocktails and Candy</Link>
             </nav>
-            <nav className="right-menu"></nav>
 
-            <nav>
+            <nav className="right-menu">
                 <Link className="nav-button" to="/">Home</Link>
                 <Link className="nav-button" to="/cocktails">Cocktail</Link>
             </nav>
